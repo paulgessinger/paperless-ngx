d70d83668f35719e2789d19174e181412545a2a5
hendrik1120 <89412959+hendrik1120@users.noreply.github.com>
2025-03-31 00:03:30 +0200

    feat(oidc): add support for app oidc

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -332,6 +332,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.mfa",
+    "allauth.headless",
     "drf_spectacular",
     "drf_spectacular_sidecar",
     "treenode",
@@ -513,6 +514,7 @@ SOCIALACCOUNT_PROVIDERS = json.loads(
 )
 SOCIAL_ACCOUNT_DEFAULT_GROUPS = __get_list("PAPERLESS_SOCIAL_ACCOUNT_DEFAULT_GROUPS")
 SOCIAL_ACCOUNT_SYNC_GROUPS = __get_boolean("PAPERLESS_SOCIAL_ACCOUNT_SYNC_GROUPS")
+HEADLESS_TOKEN_STRATEGY = "paperless.strategies.DrfTokenStrategy"
 
 MFA_TOTP_ISSUER = "Paperless-ngx"
 
--- src/paperless/urls.py
+++ src/paperless/urls.py
@@ -349,6 +349,7 @@ urlpatterns = [
             ],
         ),
     ),
+    path("_allauth/", include("allauth.headless.urls")),
     # Root of the Frontend
     re_path(
         r".*",
