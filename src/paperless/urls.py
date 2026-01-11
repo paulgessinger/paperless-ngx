eb8c1051cd5cb6008028fde4fc09aa56f2608076
Paul Gessinger <hello@paulgessinger.com>
2026-01-11 20:20:43 +0100

    swithc url from _allauth to api/oidc

--- src/paperless/urls.py
+++ src/paperless/urls.py
@@ -349,7 +349,7 @@ urlpatterns = [
             ],
         ),
     ),
-    path("_allauth/", include("allauth.headless.urls")),
+    path("api/oidc/", include("allauth.headless.urls")),
     # Root of the Frontend
     re_path(
         r".*",
